712782fbc472754c0a936f2d84067799ead4c9d8
Lassi <joffrey.lassignardie@gmail.com>
2025-02-13 20:10:52 +1100

    Tried to move requirement test to e2e but seems flaky

--- src/e2e/create-race.spec.js
+++ src/e2e/create-race.spec.js
@@ -4,29 +4,44 @@ test('Teacher can create a new race', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   // RootPage
-  expect(page.getByRole('heading', 'All your races')).toBeVisible();
+  await expect(page.getByRole('heading', 'All your races')).toBeVisible();
 
   await page.getByRole('link', { name: 'Create new race' }).click();
 
   // NewRacePage
-  expect(page.getByRole('heading', 'Create a new race')).toBeVisible();
+  await expect(page.getByRole('heading', 'Create a new race')).toBeVisible();
 
   const studentInput = page.getByRole('textbox');
-  const studentButton = page.getByRole('button', { name: 'Add student' });
+  const addStudentButton = page.getByRole('button', { name: 'Add student' });
 
   const studentNames = ['Mike Bottle', 'Tommy Pickle', 'Chucky Something', 'Coco Apple'];
 
   for (const studentName of studentNames) {
     await studentInput.fill(studentName);
-    await studentButton.click();
+    await addStudentButton.click();
   }
 
-  await page.getByRole('button', { name: 'Create' }).click();
+  await page.getByRole('button', { name: 'Create race' }).click();
 
   // RacePage
   await page.waitForURL('**/races/*');
 
-  expect(page.getByRole('heading', 'Temporary race page')).toBeVisible();
+  await expect(page.getByRole('heading', 'Temporary race page')).toBeVisible();
 
-  expect(page.getByRole('listitem')).toHaveText(studentNames);
+  await expect(page.getByRole('listitem')).toHaveText(studentNames);
+});
+
+test('Teacher cannot create a race with less than 2 students', async ({ page }) => {
+
+  await page.goto('http://localhost:3000/races/new');
+
+  const studentName = 'Lone Student';
+  const studentInput = page.getByRole('textbox');
+  const addStudentButton = page.getByRole('button', { name: 'Add student' });
+
+  await studentInput.fill(studentName);
+  await expect(addStudentButton).toBeEnabled();
+  await addStudentButton.click();
+
+  await expect(page.getByRole('button', { name: 'Create race' })).toBeDisabled();
 });
