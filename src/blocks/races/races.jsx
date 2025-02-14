217399f3f8e809029dfb91ea2c530d3099c49545
Lassi <joffrey.lassignardie@gmail.com>
2025-02-14 14:38:19 +1100

    Glow up main page

--- src/blocks/races/races.jsx
+++ src/blocks/races/races.jsx
@@ -20,28 +20,30 @@ export const RacesBlock = ({ liveRaces, completedRaces }) => {
           <Link href="/races/new">Create new race</Link>
         </Button>
       </Navbar>
-      <main className="px-8 py-4">
-        <h1 className="text-4xl">All your races</h1>
-        <Tabs className="md:w-1/2" defaultValue="live">
-          <TabsList className="grid sm:w-1/2 md:w-1/3 lg:w-1/4 grid-cols-2">
-            <TabsTrigger value="live">Live</TabsTrigger>
-            <TabsTrigger value="completed">Completed</TabsTrigger>
-          </TabsList>
-          <TabsContent value="live">
-            <RaceList>
-              {liveRaces.map((race) => (
-                <RaceListItem key={race.id} id={race.id} name={race.name} numberOfStudents={race.students.length}/>
-              ))}
-            </RaceList>
-          </TabsContent>
-          <TabsContent value="completed">
-            <RaceList>
-              {completedRaces.map((race) => (
-                <RaceListItem key={race.id} id={race.id} name={race.name} numberOfStudents={race.students.length}/>
-              ))}
-            </RaceList>
-          </TabsContent>
-        </Tabs>
+      <main className="px-8 py-4 flex flex-col">
+        <div className="flex flex-col gap-6 md:self-center md:w-2/3 lg:w-1/2">
+          <h1 className="text-4xl">All your races</h1>
+          <Tabs defaultValue="live">
+            <TabsList className="grid sm:w-1/3 grid-cols-2">
+              <TabsTrigger value="live">Live</TabsTrigger>
+              <TabsTrigger value="completed">Completed</TabsTrigger>
+            </TabsList>
+            <TabsContent value="live">
+              <RaceList>
+                {liveRaces.map((race) => (
+                  <RaceListItem key={race.id} id={race.id} name={race.name} numberOfStudents={race.students.length}/>
+                ))}
+              </RaceList>
+            </TabsContent>
+            <TabsContent value="completed">
+              <RaceList>
+                {completedRaces.map((race) => (
+                  <RaceListItem key={race.id} id={race.id} name={race.name} numberOfStudents={race.students.length}/>
+                ))}
+              </RaceList>
+            </TabsContent>
+          </Tabs>
+        </div>
       </main>
     </>
   );
